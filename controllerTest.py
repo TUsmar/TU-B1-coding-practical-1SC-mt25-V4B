f316064e70b64c3a48a8e589397674e6b2acbdea
Thomas Usmar <thomas@usmars.com>
2025-10-22 20:10:54 +0100

    move contoller into a seperate python file

---
+++ controllerTest.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pt
+from uuv_mission.dynamic import Mission, Submarine, ClosedLoop, Trajectory
+from uuv_mission.Controller import controller
+#from uuv_mission.terrain import plot_reference_and_terrain #not used currently
+#functions
+# Create mission from CSV
+def load_saved_missionProfile():
+    mission = Mission.from_csv('data/mission.csv')
+    return(mission)
+# Create random mission
+def generate_random_mission():
+    mission = Mission.random_mission(duration=160, scale=2.0)
+    #plot_reference_and_terrain(mission.reference, mission.cave_height, mission.cave_depth)
+    return(mission)
+
+# Create submarine instance
+sub = Submarine()
+
+# Create closed loop instance with submarine
+closed_loop = ClosedLoop(sub)
+
+# Setup simulation parameters
+disturbance_noise=0.5
+
+#generate the mission
+mission=load_saved_missionProfile()
+
+#simulate and plot the mission
+Trajectory.plot_completed_mission(ClosedLoop.simulate_with_random_disturbances(closed_loop, mission, variance=disturbance_noise), mission)
